021715d3b5c2f34f5abed74b9c754dda780b928b
Teddy Lattanzio <teddy@148-100-163-101.foxnet.marist.edu>
2022-09-19 20:58:28 -0400

    adding labwork

---
+++ assignments/basics/primitives.py
@@ -0,0 +1,23 @@
+def get_int():
+    '''
+    Returns a variable of type 'int'.
+    '''
+    return 5
+
+def get_float():
+    '''
+    Returns a variable of type 'float'.
+    '''
+    return 6.5
+
+def get_str():
+    '''
+    Returns a variable of type 'str'.
+    '''
+    return "fuck"
+
+def get_bool():
+    '''
+    Returns a variable of type 'bool'.
+    '''
+    return False
